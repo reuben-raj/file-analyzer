c73bb62025c726f127922b284a863cf1f60d65ab
reuben-raj <reuben.raj90@gmail.com>
2022-05-25 15:51:36 +0800

    Initial commit

---
+++ src/main/java/com/illumina/fileanalyzer/FileAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.illumina.fileanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileAnalyzerApplication.class, args);
+    }
+
+}
